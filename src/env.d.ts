79a2a1e69790c5b3c37735037549a808cdb64781
Andrew Veihl <andrew@healthspaces.com>
2025-10-29 21:23:11 -0600

    1

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,6 @@
 declare module '$env/static/public' {
   export const PUBLIC_TENOR_API_KEY: string | undefined;
+  export const PUBLIC_TURN_URLS: string | undefined;
+  export const PUBLIC_TURN_USERNAME: string | undefined;
+  export const PUBLIC_TURN_CREDENTIAL: string | undefined;
 }
