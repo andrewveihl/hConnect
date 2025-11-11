fd547b81e4102402d4e68eab797abacfdba35113
Andrew Veihl <andrew@healthspaces.com>
2025-11-10 23:07:29 -0700

    ;

--- src/env.d.ts
+++ src/env.d.ts
@@ -7,7 +7,6 @@ declare module '$env/static/public' {
   export const PUBLIC_FIREBASE_STORAGE_BUCKET: string | undefined;
   export const PUBLIC_FIREBASE_STORAGE_BUCKET_URL: string | undefined;
   export const PUBLIC_FCM_VAPID_KEY: string | undefined;
-  export const PUBLIC_AI_ENDPOINT: string | undefined;
   export const PUBLIC_TENOR_API_KEY: string | undefined;
   export const PUBLIC_TURN_URLS: string | undefined;
   export const PUBLIC_TURN_USERNAME: string | undefined;
