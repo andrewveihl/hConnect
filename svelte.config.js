94de0ab606a6cdc38a4ae6850451d96ab19212f9
Simon Green <simon@captaincodeman.com>
2026-02-10 15:42:15 -0700

    enable svelte inspector
    helps locate source files quickly

--- svelte.config.js
+++ svelte.config.js
@@ -3,19 +3,22 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-  compilerOptions: {
-    runes: true
-  },
-  preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter({
-      fallback: 'index.html',
-      pages: 'build',
-      assets: 'build',
-      precompress: false
-    }),
-    prerender: { entries: [] }
-  }
+	compilerOptions: {
+		runes: true
+	},
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter({
+			fallback: 'index.html',
+			pages: 'build',
+			assets: 'build',
+			precompress: false
+		}),
+		prerender: { entries: [] }
+	},
+	vitePlugin: {
+		inspector: true
+	}
 };
 
 export default config;
